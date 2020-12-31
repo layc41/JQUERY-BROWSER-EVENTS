e0f2832fd168e84161625184db65310e2e8da57c
layc41 <64671431+layc41@users.noreply.github.com>
2020-12-31 10:37:05 -0600

    worked on city pages

--- js/home.js
+++ js/home.js
@@ -1,3 +1,62 @@
 $(document).ready(function () {
+    // initial load
+    $('#akronInfoDiv').hide();
+    $('#minneapolisInfoDiv').hide();
+    $('#louisvilleInfoDiv').hide();
+
+    // main tab
+    $('#mainButton').on('click', function () {
+        $('#mainInfoDiv').show();
+        $('#louisvilleInfoDiv').hide();
+        $('#akronInfoDiv').hide();
+        $('#minneapolisInfoDiv').hide();
+    })
     
+    // akron tab
+    $('#akronButton').on('click', function () {
+        $('#akronInfoDiv').show();
+        $('#akronWeather').hide();
+        $('#mainInfoDiv').hide();
+        $('#minneapolisInfoDiv').hide();
+        $('#louisvilleInfoDiv').hide();
+    })
+
+    $('#akronWeatherButton').on('click', function () {
+        $('#akronWeather').toggle();
+    })
+    
+    // minneapolis tab
+    $('#minneapolisButton').on('click', function () {
+        $('#minneapolisInfoDiv').show();
+        $('#minneapolisWeather').hide();
+        $('#akronInfoDiv').hide();
+        $('#mainInfoDiv').hide();
+        $('#louisvilleInfoDiv').hide();
+    })
+
+    $('#minneapolisWeatherButton').on('click', function () {
+        $('#minneapolisWeather').toggle();
+    })
+    
+    // louisville tab
+    $('#louisvilleButton').on('click', function () {
+        $('#louisvilleInfoDiv').show();
+        $('#louisvilleWeather').hide();
+        $('#akronInfoDiv').hide();
+        $('#mainInfoDiv').hide();
+        $('#minneapolisInfoDiv').hide();
+    })
+
+    $('#louisvilleWeatherButton').on('click', function () {
+        $('#louisvilleWeather').toggle();
+    })
+
+    $('td').hover( function () {
+        // in callback
+        $(this).parent.css("backgound-color", "WhiteSmoke");
+
+        // out callback
+        $(this).parent.css("backgound-color", "White");
+    })
+
 });
